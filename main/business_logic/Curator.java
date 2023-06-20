e6229830934e569b88f572cef8ab21ec9fb9263c
Gio <gioformichella@gmail.com>
2023-06-20 18:41:35 +0200

    Schema classe Curator

---
+++ main/business_logic/Curator.java
@@ -0,0 +1,23 @@
+package main.business_logic;
+
+import main.DomainModel.*;
+
+import java.util.List;
+
+
+public class Curator {
+    private String emailAddress = null; //va aggiunta un email museale
+
+    public void addItinerary(int id){}
+
+    public void addArtwork(Itinerary i, Artwork a){}
+
+    public List<Artwork> viewArtworks(){
+        return null;
+    }
+
+    public void modifyStatus(Artwork a, ArtworkStatus as){}
+
+    public void cancelItinerary(){}
+
+}
