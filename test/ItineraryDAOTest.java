f3061f2591a38c0741dd29291a092bd2d544619e
Gio <gioformichella@gmail.com>
2023-06-21 18:21:33 +0200

    Test parziale per metodo delete

--- test/ItineraryDAOTest.java
+++ test/ItineraryDAOTest.java
@@ -1,14 +1,16 @@
 package test;
 
 import main.DomainModel.Itinerary;
+import main.orm.ConnectionManager;
 import main.orm.ItineraryDAO;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ItineraryDAOTest {
 
@@ -35,7 +37,30 @@ public class ItineraryDAOTest {
             }
         }
     }
+
     @Test
     public void testDelete() {
+        ItineraryDAO dao = new ItineraryDAO();
+        Itinerary i = new Itinerary(2, "RomaAntica", new ArrayList<>());
+
+        try {
+            dao.insert(i);
+            //TODO metodo addArtwork(Artwork, Itinerary) aggiunge relazione opera itinerario, delete rimuove anche questa
+            dao.delete(i);
+            assertNull(dao.getTransitive(i.getId()));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {  //if something went wrong with delete method, manual deletion of inserted tuple
+                Connection con = ConnectionManager.getConnection();
+                String sql1 = "DELETE FROM Itinerary WHERE id = ?";
+                PreparedStatement ps1 = con.prepareStatement(sql1);
+                ps1.setInt(1, i.getId());
+                ps1.executeUpdate();
+                ps1.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
