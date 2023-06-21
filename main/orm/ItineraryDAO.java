6ad49288af157ac15c5fbdbe03a9ced4350c23e9
Gio <gioformichella@gmail.com>
2023-06-21 11:47:42 +0200

    fixed bug in delete method and added get method

--- main/orm/ItineraryDAO.java
+++ main/orm/ItineraryDAO.java
@@ -4,7 +4,9 @@ import main.DomainModel.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ItineraryDAO {
     public void insert(Itinerary i) throws SQLException {
@@ -28,10 +30,43 @@ public class ItineraryDAO {
         ps1.executeUpdate();
         ps1.close();
 
-        //rimuove associazione con le opere d'arte
-        String sql2 = "DELETE FROM Artwork_Itinerary WHERE Itinerary = ?";
+        //removes association with artworks
+        String sql2 = "DELETE FROM Artwork_Itinerary WHERE itinerary = ?";
         PreparedStatement ps2 = con.prepareStatement(sql2);
         ps2.setInt(1, i.getId());
+        ps2.executeUpdate();
         ps2.close();
+    }
+
+    public Itinerary getTransitive(int id) throws SQLException {
+        //function also instantiates Artwork objects
+
+        Connection con = ConnectionManager.getConnection();
+
+        String sql = "SELECT id, Itinerary.name i_name, code, Artwork.name a_name, artist author, status, payload FROM Itinerary, Artwork_Itinerary, Artwork WHERE id = ? , Artwork_Itinerary.itinerary = id, code = Artwork_Itinerary.artwork";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+
+        ArrayList<Artwork> artworks = new ArrayList<>();
+        while (rs.next()) {
+            ArtworkStatus as = null;
+            switch (rs.getInt("status")) {
+                //status integers defined in DB
+                case 1:
+                    as = new OnDisplay();
+                    break;
+                case 2:
+                    as = new UnderMaintainance(rs.getString("payload"));
+                    break;
+                case 3:
+                    as = new OnLoan(rs.getString("payload"));
+                    break;
+            }
+            Artwork a = new Artwork(rs.getInt("code"), rs.getString("a_name"), rs.getString("author"), as);
+            artworks.add(a);
+        }
+
+        return new Itinerary(rs.getInt("id"), rs.getString("i_name"), artworks);
     }
 }
