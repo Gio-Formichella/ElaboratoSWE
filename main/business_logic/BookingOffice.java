fb8620d6e68f7f3fef04fec9b964dae6fd40e7a3
Davide <davide.lombardi2@stud.unifi.it>
2023-07-12 16:13:13 +0200

    Aggiunto il Singleton Notifier per l'invio delle mail

--- main/business_logic/BookingOffice.java
+++ main/business_logic/BookingOffice.java
@@ -22,8 +22,6 @@ import javax.mail.internet.MimeMultipart;
 
 public class BookingOffice {
 
-    private final String emailAddress = "museoswe@gmail.com";
-    private final String emailPassword = "usomdxtqjcwbdiid";
 
     public void setVisit(int code, String date, String time, int maxVisitors, float price, String language, ArrayList<Itinerary> itineraries) throws SQLException, ParseException {
         if (maxVisitors < 0 || price < 0) {
@@ -40,6 +38,7 @@ public class BookingOffice {
         VisitorDAO vdao = new VisitorDAO();
         ArrayList<Visitor> toBeNotifiedVisitors = vdao.getToBeNotifiedVisitors(v);
         StringBuilder itinerariesMessage = new StringBuilder();
+        Notifier notifier = Notifier.getInstance();
         for (Itinerary i : v.getItineraries()) {
             itinerariesMessage.append(i.getName()).append(" ");
         }
@@ -47,7 +46,7 @@ public class BookingOffice {
         dao.delete(code);
         //invio email
         if (toBeNotifiedVisitors.size() > 0) {
-            sendEmail(toBeNotifiedVisitors, "Visit has been canceld", messageToSend);
+            notifier.sendEmail(toBeNotifiedVisitors, "Visit has been canceld", messageToSend);
         }
     }
 
@@ -55,6 +54,7 @@ public class BookingOffice {
         VisitDAO dao = new VisitDAO();
         Visit vOld = dao.getTransitive(v.getCode());
         StringBuilder oldItinerariesMessage = new StringBuilder();
+        Notifier notifier = Notifier.getInstance();
         for (Itinerary i : v.getItineraries()) {
             oldItinerariesMessage.append(i.getName()).append(" ");
         }
@@ -66,7 +66,7 @@ public class BookingOffice {
         //invio email
         if (toBeNotifiedVisitors.size() > 0) {
             String messageToSend = oldVisitMessage + " has been changed. The visit will be held the " + v.getDate() + " and start at " + v.getTime() + " and conducted in " + v.getLanguage();
-            sendEmail(toBeNotifiedVisitors, "Visit change", messageToSend);
+            notifier.sendEmail(toBeNotifiedVisitors, "Visit change", messageToSend);
         }
     }
 
@@ -75,53 +75,5 @@ public class BookingOffice {
         return dao.getAll();
     }
 
-    private void sendEmail(ArrayList<Visitor> toBeNotified, String subject, String messageToSend) throws MessagingException {
-        Properties properties = new Properties();
-        properties.put("mail.smtp.auth", "true");  //autenticazione user
-        properties.put("mail.smtp.host", "smtp.gmail.com");  //server smtp gmail
-        properties.put("mail.smtp.port", "587"); //numero di porta richiesto da gmail
-        properties.put("mail.smtp.starttls.enable", "true");
 
-        Session session = Session.getInstance(properties, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(emailAddress, emailPassword);
-            }
-        });
-
-        Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(emailAddress));
-        message.setSubject(subject);
-        for (Visitor v : toBeNotified) {
-            Address addressTo = new InternetAddress(v.getEmailAddress());
-            message.addRecipient(Message.RecipientType.TO, addressTo);
-        }
-        message.setSubject(subject);
-
-        MimeMultipart multipart = new MimeMultipart("related");
-
-        //first part of the message
-        BodyPart messageBodyPart = new MimeBodyPart();
-        String htmlText = "<img src=\"cid:image\" alt=\"Museo di SWE\" style=\"width: 300px; height: 100px; \">\r\n" + //
-                "<h5 style=\"color: gray; font-family: Arial,sans-serif\">" + messageToSend + "</h5>\r\n" + //
-                "<div style=\"margin-top: 5em\">\r\n" + //
-                "  <p style=\"color: gray; font-family: Arial,sans-serif; font-size: 0.7em\">Contatti:</p>\r\n" + //
-                "  <p style=\"color: gray; font-family: Arial,sans-serif; font-size: 0.7em\">Telefono: 1234567890</p>\r\n" + //
-                "  <p style=\"color: gray; font-family: Arial,sans-serif; font-size: 0.7em\">Email: museoswe@gmail.com</p>\r\n" + //
-                "</div>";
-        messageBodyPart.setContent(htmlText, "text/html");
-        multipart.addBodyPart(messageBodyPart);
-
-        //second part of the message
-        messageBodyPart = new MimeBodyPart();
-        FileDataSource fds = new FileDataSource("imgs/logo.png");
-        messageBodyPart.setDataHandler(new DataHandler(fds));
-        messageBodyPart.setHeader("Content-ID", "<image>");
-        messageBodyPart.setFileName("logo.png");
-        multipart.addBodyPart(messageBodyPart);
-
-        message.setContent(multipart);
-
-        Transport.send(message);
-    }
 }
